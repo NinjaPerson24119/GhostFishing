1e0fb55f6edb1d2fd07577d7c178b63fc42c886f
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-22 10:50:10 -0600

    move gerstner into separate shader include

--- common/ocean/Ocean.cs
+++ common/ocean/Ocean.cs
@@ -20,7 +20,7 @@ public partial class Ocean : MeshInstance3D {
 		material.SetShaderParameter("height_scale", height_scale);
 		material.SetShaderParameter("time_scale", time_scale);
 
-		float[] gerstner_amplitude = new float[] { 0.5f, 0.25f, 0.05f };
+		float[] gerstner_amplitude = new float[] { 0.1f, 0.25f, 0.05f };
 		float[] gerstner_phi = new float[] { 0, Mathf.Pi * 1f, Mathf.Pi * 0.5f };
 		float[] gerstner_k_x = new float[] { Mathf.Pi * 0.25f, Mathf.Pi * 0.5f, Mathf.Pi * 1.2f };
 		float[] gerstner_k_z = new float[] { Mathf.Pi * 0.25f, Mathf.Pi * 0.7f, Mathf.Pi * 1f };
