52beb54b1b8cf457c5e9cac31b5a49a1f6f9cc86
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-28 13:34:36 -0600

    impl plane approximation to waves

--- entities/player/Player.cs
+++ entities/player/Player.cs
@@ -56,50 +56,68 @@ public partial class Player : RigidBody3D {
     }
 
     public override void _IntegrateForces(PhysicsDirectBodyState3D state) {
-        state.LinearVelocity *= 1 - WaterDrag;
+        // we need to zero out the Y component of the velocity because we don't physics to move the boat up and down
+        float waterDragFactor = 1 - WaterDrag;
+        state.LinearVelocity = new Vector3(state.LinearVelocity.X * waterDragFactor, 0, state.LinearVelocity.Z * waterDragFactor);
+
         state.AngularVelocity *= 1 - WaterAngularDrag;
 
         //Vector3 waterPositionDisplacement = _ocean.GetDisplacement(new Vector2(GlobalPosition.X, GlobalPosition.Z));
         //float boatY = _ocean.GlobalPosition.Y + waterPositionDisplacement.Y + _absBounds.Size.Y / 2 - BoatDepth;
         //GlobalPosition = new Vector3(GlobalPosition.X, boatY, GlobalPosition.Z);
 
-        ComputeBoatRotation();
+        var angles = EstimatePitchRollWithWaves();
+        GlobalRotation = new Vector3(angles.pitch, GlobalRotation.Y, angles.roll);
     }
 
-    public Vector2 ComputeBoatRotation() {
+    public (float pitch, float roll) EstimatePitchRollWithWaves() {
         // get the points around the boat AABB in cross shape (+)
         float halfWidth = _absBounds.Size.X / 2;
         float halfLength = _absBounds.Size.Z / 2;
-        Vector3 front = new Vector3(GlobalPosition.X, 0, GlobalPosition.Z + halfLength).Rotated(Vector3.Up, GlobalRotation.Y);
-        Vector3 back = new Vector3(GlobalPosition.X, 0, GlobalPosition.Z - halfLength).Rotated(Vector3.Up, GlobalRotation.Y);
-        Vector3 left = new Vector3(GlobalPosition.X - halfWidth, 0, GlobalPosition.Z).Rotated(Vector3.Up, GlobalRotation.Y);
-        Vector3 right = new Vector3(GlobalPosition.X + halfWidth, 0, GlobalPosition.Z).Rotated(Vector3.Up, GlobalRotation.Y);
+        Vector3 GlobalPositionXZ = new Vector3(GlobalPosition.X, 0, GlobalPosition.Z);
+        Vector3 front = new Vector3(0, 0, halfLength).Rotated(Vector3.Up, GlobalRotation.Y) + GlobalPositionXZ;
+        Vector3 back = new Vector3(0, 0, -halfLength).Rotated(Vector3.Up, GlobalRotation.Y) + GlobalPositionXZ;
+        Vector3 left = new Vector3(-halfWidth, 0, 0).Rotated(Vector3.Up, GlobalRotation.Y) + GlobalPositionXZ;
+        Vector3 right = new Vector3(halfWidth, 0, 0).Rotated(Vector3.Up, GlobalRotation.Y) + GlobalPositionXZ;
+
+        // adjust the points to the ocean surface
+        float averageDisplacementY = 0;
+        Vector3[] pointsToDisplace = { front, back, left, right };
+        for (int i = 0; i < pointsToDisplace.Length; i++) {
+            Vector3 displacement = _ocean.GetDisplacement(new Vector2(pointsToDisplace[i].X, pointsToDisplace[i].Z));
+            pointsToDisplace[i].Y += displacement.Y;
+            averageDisplacementY += displacement.Y;
+        }
+        averageDisplacementY /= pointsToDisplace.Length;
+        front = pointsToDisplace[0];
+        back = pointsToDisplace[1];
+        left = pointsToDisplace[2];
+        right = pointsToDisplace[3];
+
         GetNode<MeshInstance3D>("WaterApproximation/FrontSphere").GlobalPosition = front;
         GetNode<MeshInstance3D>("WaterApproximation/BackSphere").GlobalPosition = back;
         GetNode<MeshInstance3D>("WaterApproximation/LeftSphere").GlobalPosition = left;
         GetNode<MeshInstance3D>("WaterApproximation/RightSphere").GlobalPosition = right;
 
-        // adjust the points to the ocean surface
-        Vector3[] pointsToDisplace = { front, back, left, right };
-        for (int i = 0; i < pointsToDisplace.Length; i++) {
-            Vector3 displacement = _ocean.GetDisplacement(new Vector2(pointsToDisplace[i].X, pointsToDisplace[i].Z));
-            pointsToDisplace[i].Y += displacement.Y;
-        }
-        GetNode<MeshInstance3D>("WaterApproximation/FrontSphere").GlobalPosition = pointsToDisplace[0];
-        GetNode<MeshInstance3D>("WaterApproximation/BackSphere").GlobalPosition = pointsToDisplace[1];
-        GetNode<MeshInstance3D>("WaterApproximation/LeftSphere").GlobalPosition = pointsToDisplace[2];
-        GetNode<MeshInstance3D>("WaterApproximation/RightSphere").GlobalPosition = pointsToDisplace[3];
-
         // compute the rotation
-        Vector3 frontToBack = back - front;
-        Vector3 leftToRight = right - left;
-        Vector3 normal = frontToBack.Cross(leftToRight).Normalized();
-        float pitch = Mathf.Acos(normal.Dot(Vector3.Forward));
-        float roll = Mathf.Acos(normal.Dot(Vector3.Right));
+        //Vector3 normal = frontToBack.Cross(leftToRight).Normalized();
+        float angleFront = Mathf.Acos(front.Normalized().Dot(GlobalTransform.Basis.Z));
+        float angleBack = Mathf.Acos(back.Normalized().Dot(-GlobalTransform.Basis.Z));
+        float angleLeft = Mathf.Acos(left.Normalized().Dot(-GlobalTransform.Basis.X));
+        float angleRight = Mathf.Acos(right.Normalized().Dot(GlobalTransform.Basis.X));
+
+        // assign outputs
+        float pitch = (angleFront + angleBack) / 2;
+        float roll = (angleLeft + angleRight) / 2;
 
         GD.Print($"pitch: {Mathf.RadToDeg(pitch)}, roll: {Mathf.RadToDeg(roll)}");
-        //GetNode<MeshInstance3D>("WaterApproximation/WaterPlane").GlobalRotation = new Vector3(pitch, GlobalRotation.Y, roll);
+        var waterPlane = GetNode<MeshInstance3D>("WaterApproximation/WaterPlane");
 
-        return Vector2.Zero;
+        waterPlane.Position = new Vector3(0, averageDisplacementY, 0);
+        waterPlane.Rotation = new Vector3(pitch, GlobalRotation.Y, roll);
+        // BUG: why does changing the rotation reset the scale?
+        waterPlane.Scale = new Vector3(2.5f, 1, 4.5f);
+
+        return (pitch, roll);
     }
 }
