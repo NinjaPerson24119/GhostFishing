11376a9337fbb3402e1102627daba093e300c41c
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-30 17:46:37 -0600

    feat: use the simple matrix to rotate inventory items

--- common/inventory/InventoryItem.cs
+++ common/inventory/InventoryItem.cs
@@ -11,53 +11,35 @@ public class InventoryItemSpatial {
     public int Width { get; set; }
     public int Height { get; set; }
     public bool[] FilledMask { get; set; }
-    public InventoryItemRotation Rotation { get; set; }
-    // rotation by 90 degrees is equivalent to transposing and reversing column order
-    public InventoryItemSpatial Rotated(InventoryItemRotation rotation) {
-        InventoryItemSpatial newSpatial = new InventoryItemSpatial() {
-            Y = Y,
-            X = X,
+    public InventoryItemRotation Rotation {
+        get {
+            return _rotation;
+        }
+        set {
+            int diff = (int)value - (int)_rotation;
+            if (diff < 0) {
+                diff += 4;
+            }
+            diff = diff % 4;
+            for (int i = 0; i < diff; i++) {
+                RotateClockwise90();
+            }
+        }
+    }
+    InventoryItemRotation _rotation = InventoryItemRotation.None;
+    public void RotateClockwise90() {
+        Matrix<bool> filledMaskMatrix = new Matrix<bool>() {
             Width = Width,
             Height = Height,
-            FilledMask = new bool[Width * Height],
-            Rotation = rotation,
+            Data = FilledMask,
         };
-
-        // not necessarily square, so swap dims on uneven rotation
-        int noRotations = (int)rotation;
-        if (noRotations % 2 != 0) {
-            newSpatial.Width = Height;
-            newSpatial.Height = Width;
-        }
-        return newSpatial;
-    }
-    private RotateClockwise90(bool[] matrix) {
-        // transpose
-        for (int y = 0; y < Height; y++) {
-            for (int x = 0; x < Width; x++) {
-                newSpatial.FilledMask[x * Height + y] = FilledMask[y * Width + x];
-            }
-        }
-
-        // reverse columns
-        for (int y = 0; y < newSpatial.Height; y++) {
-            for (int x = 0; x < newSpatial.Width / 2; x++) {
-                int leftIdx = y * newSpatial.Width + x;
-                int rightIdx = y * newSpatial.Width + newSpatial.Width - 1 - x;
-                bool temp = newSpatial.FilledMask[leftIdx];
-                newSpatial.FilledMask[leftIdx] = newSpatial.FilledMask[rightIdx];
-                newSpatial.FilledMask[rightIdx] = temp;
-            }
-        }
+        filledMaskMatrix.RotateClockwise90();
+        FilledMask = filledMaskMatrix.Data;
     }
 }
 
 public class InventoryItem {
     public string UUID { get; set; }
     public InventoryItemSpatial Spatial { get; set; }
-
-    // TODO: support rotation
-    // - Store rotation
-    // - Get rotated spatial
 }
 
