c617a758ea490bdbb193d11883509044a24811c1
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-30 17:46:23 -0600

    feat: simple matrix class for rotating data

---
+++ common/util/Matrix.cs
@@ -0,0 +1,36 @@
+public struct Matrix<T> {
+    public int Width;
+    public int Height;
+    public T[] Data;
+
+    public void Transpose() {
+        T[] newData = new T[Width * Height];
+        for (int y = 0; y < Height; y++) {
+            for (int x = 0; x < Width; x++) {
+                newData[x * Height + y] = Data[y * Width + x];
+            }
+        }
+
+        int temp = Width;
+        Width = Height;
+        Height = temp;
+        Data = newData;
+    }
+
+    public void ReverseColumns() {
+        for (int y = 0; y < Height; y++) {
+            for (int x = 0; x < Width / 2; x++) {
+                int leftIdx = y * Width + x;
+                int rightIdx = y * Width + Width - 1 - x;
+                T temp = Data[leftIdx];
+                Data[leftIdx] = Data[rightIdx];
+                Data[rightIdx] = temp;
+            }
+        }
+    }
+
+    public void RotateClockwise90() {
+        Transpose();
+        ReverseColumns();
+    }
+}
