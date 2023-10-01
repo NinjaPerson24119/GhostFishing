430cf9008a759927be69525b9175311cea8ebb5c
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-01 15:40:44 -0600

    feat: script to mirror svgs to consumable png directory

--- artwork/generate_pngs.sh
+++ artwork/generate_pngs.sh
@@ -8,13 +8,22 @@ mkdir $dest_dir
 
 process_file() {
     local src_file="$1"
-    local dest_file="$dest_dir/${src_file#$src_dir/}"
-    local dest_file_svg=$(echo "$dest_file" | sed 's/\.svg$/.png/')
+    if [[ ! $src_file =~ \.svg$ ]]; then
+        echo "Skipping: $src_file: not an SVG file"
+        return
+    fi
 
-    mkdir -p "${dirname "$dest_file_svg"}"
+    local dest_file="$dest_dir${src_file#$src_dir}"
+    local dest_file_svg="$(echo "$dest_file" | sed 's/\.svg$/.png/')"
+    echo "Converting: $src_file -> $dest_file_svg"
+
+    mkdir -p $(dirname $dest_file_svg)
     inkscape --export-type="png" -o "$dest_file_svg" "$src_file"
 
     echo "Converted: $src_file -> $dest_file"
 }
 
-find "$src_dir" -type f -exec bash -c 'process_file "$0"' {} \;
+file_list=$(find "$src_dir" -type f)
+for file in $file_list; do
+    process_file "$file"
+done
