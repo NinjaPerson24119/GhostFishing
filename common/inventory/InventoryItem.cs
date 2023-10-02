850941d8bc9a33e9aa298c40cce19770f7d8fe7d
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-01 22:54:06 -0600

    feat: InventoryItem precomputes all FilledMask rotations

--- common/inventory/InventoryItem.cs
+++ common/inventory/InventoryItem.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 public enum InventoryItemRotation {
     None = 0,
     Clockwise90 = 1,
@@ -10,31 +12,47 @@ public class InventoryItemSpatial {
     public int X { get; set; }
     public int Width { get; set; }
     public int Height { get; set; }
-    public bool[] FilledMask { get; set; }
-    public InventoryItemRotation Rotation {
+    public InventoryItemRotation Rotation = InventoryItemRotation.None;
+
+    [JsonPropertyName("FilledMask")]
+    private bool[] _filledMask {
         get {
-            return _rotation;
+            return _filledMaskClockwise0;
         }
         set {
-            int diff = (int)value - (int)_rotation;
-            if (diff < 0) {
-                diff += 4;
-            }
-            diff = diff % 4;
-            for (int i = 0; i < diff; i++) {
-                RotateClockwise90();
-            }
+            _filledMaskClockwise0 = value;
+            Matrix<bool> filledMaskMatrix = new Matrix<bool>() {
+                Width = Width,
+                Height = Height,
+                Data = value,
+            };
+
+            // Clone() is shallow copy, but bool is not a reference type, so it's OK
+            filledMaskMatrix.RotateClockwise90();
+            _filledMaskClockwise90 = filledMaskMatrix.Data.Clone() as bool[];
+            filledMaskMatrix.RotateClockwise90();
+            _filledMaskClockwise180 = filledMaskMatrix.Data.Clone() as bool[];
+            filledMaskMatrix.RotateClockwise90();
+            _filledMaskClockwise270 = filledMaskMatrix.Data.Clone() as bool[];
         }
     }
-    InventoryItemRotation _rotation = InventoryItemRotation.None;
-    public void RotateClockwise90() {
-        Matrix<bool> filledMaskMatrix = new Matrix<bool>() {
-            Width = Width,
-            Height = Height,
-            Data = FilledMask,
-        };
-        filledMaskMatrix.RotateClockwise90();
-        FilledMask = filledMaskMatrix.Data;
+    private bool[] _filledMaskClockwise0 { get; set; }
+    private bool[] _filledMaskClockwise90;
+    private bool[] _filledMaskClockwise180;
+    private bool[] _filledMaskClockwise270;
+    public bool[] GetFilledMask() {
+        switch (Rotation) {
+            case InventoryItemRotation.None:
+                return _filledMaskClockwise0;
+            case InventoryItemRotation.Clockwise90:
+                return _filledMaskClockwise90;
+            case InventoryItemRotation.Clockwise180:
+                return _filledMaskClockwise180;
+            case InventoryItemRotation.Clockwise270:
+                return _filledMaskClockwise270;
+            default:
+                throw new System.ArgumentException("Invalid rotation");
+        }
     }
 }
 
