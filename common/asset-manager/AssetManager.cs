83080dd386f0937eb928934c46a9f76a3ee9d43b
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-02 21:21:32 -0600

    asset manager loads all asset JSONs

--- common/asset-manager/AssetManager.cs
+++ common/asset-manager/AssetManager.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System;
 using System.IO;
 using System.Text.Json;
 using System.Collections.Generic;
@@ -19,6 +20,11 @@ public partial class AssetManager : Node {
         return _singleton;
     }
 
+    private const string _boatInventoryPath = "res://data/boat-inventory.json";
+    private const string _fishDefinitionsPath = "res://data/fish.json";
+    private const string _itemCategoryDefinitionsPath = "res://data/item-categories.json";
+    private const string _questDefinitionsPath = "res://data/quests.json";
+
     public const string TemporaryInventoryID = "INVENTORY-dc106fde-f9a0-4a99-8b58-0dee17fce491";
     public const string PlayerOneBoatInventoryID = "INVENTORY-31cdec79-2a3b-4f4d-9e23-a878915f3973";
     public const string PlayerOneQuestInventoryID = "INVENTORY-96c151e3-2436-406c-967c-79a1cc89c3ac";
@@ -26,11 +32,10 @@ public partial class AssetManager : Node {
     public const string PlayerTwoQuestInventoryID = "INVENTORY-f7091de2-6804-4f22-b8f1-d17be782adf6";
     public const string StorageInventoryID = "INVENTORY-9f5d56ed-c8aa-4899-b749-66bd734141fb";
 
-    public Dictionary<string, Inventory>? Inventories;
-    public Dictionary<string, InventoryItemDefinition>? InventoryItemDefinitions;
-    public Dictionary<string, InventoryItemCategory>? InventoryItemCategories;
-    public Dictionary<string, FishDefinition>? FishDefinitions;
-    public Dictionary<string, QuestDefinition>? QuestDefinitions;
+    public Dictionary<string, Inventory> Inventories = new Dictionary<string, Inventory>();
+    public Dictionary<string, InventoryItemDefinition> InventoryItemDefinitions = new Dictionary<string, InventoryItemDefinition>();
+    public Dictionary<string, InventoryItemCategory> InventoryItemCategories = new Dictionary<string, InventoryItemCategory>();
+    public Dictionary<string, QuestDefinition> QuestDefinitions = new Dictionary<string, QuestDefinition>();
 
     public AssetManager() {
         ProcessMode = ProcessModeEnum.Always;
@@ -41,7 +46,7 @@ public partial class AssetManager : Node {
         LoadAssets();
     }
 
-    private T LoadAssetFromJSON<T>(string filePath) {
+    private T? LoadAssetFromJSON<T>(string filePath) {
         try {
             string globalizedPath = ProjectSettings.GlobalizePath(filePath);
             string jsonString = File.ReadAllText(globalizedPath);
@@ -55,15 +60,84 @@ public partial class AssetManager : Node {
             GD.PrintErr("Error parsing JSON file: " + filePath);
             return default;
         }
+        catch (Exception e) {
+            GD.PrintErr($"Error loading asset from JSON: {filePath}. Error: {e}");
+            return default;
+        }
     }
 
     private void LoadAssets() {
-        DefaultInventory = LoadAssetFromJSON<Inventory>("res://data/default-inventory.json");
-        FishDefinitions = LoadAssetFromJSON<Dictionary<string, FishDefinition>>("res://data/fish-definitions.json");
-        foreach (var kv in FishDefinitions) {
-            GD.Print($"Fish ({kv.Key}): {kv.Value}");
+        // load initial boat inventory shape
+        LoadBoatInventories();
+
+        // TODO: instantiate temp inventory to size large enough for any pickup
+
+        LoadItemCategories();
+        LoadQuestDefinitions();
+        LoadFishDefinitions();
+    }
+
+    public void LoadBoatInventories() {
+        InventoryDTO? boatInventory = LoadAssetFromJSON<InventoryDTO>(_boatInventoryPath);
+        if (boatInventory != null) {
+            try {
+                Inventory model = new Inventory(boatInventory);
+                Inventories.Add(PlayerOneBoatInventoryID, model);
+                Inventories.Add(PlayerTwoBoatInventoryID, model);
+            }
+            catch (Exception e) {
+                GD.PrintErr($"Error loading boat inventory from DTO: {e}");
+            }
         }
     }
+
+    public void LoadItemCategories() {
+        Dictionary<string, InventoryItemCategoryDTO>? itemCategories = LoadAssetFromJSON<Dictionary<string, InventoryItemCategoryDTO>>(_itemCategoryDefinitionsPath);
+        if (itemCategories != null) {
+            try {
+                foreach (var kv in itemCategories) {
+                    InventoryItemCategory model = new InventoryItemCategory(kv.Value);
+                    InventoryItemCategories.Add(kv.Key, model);
+                }
+            }
+            catch (Exception e) {
+                GD.PrintErr($"Error loading item categories from DTO: {e}");
+            }
+        }
+    }
+
+    public void LoadQuestDefinitions() {
+        Dictionary<string, QuestDefinitionDTO>? questDefinitions = LoadAssetFromJSON<Dictionary<string, QuestDefinitionDTO>>(_questDefinitionsPath);
+        if (questDefinitions != null) {
+            try {
+                foreach (var kv in questDefinitions) {
+                    QuestDefinition model = new QuestDefinition(kv.Value);
+                    QuestDefinitions.Add(kv.Key, model);
+                }
+            }
+            catch (Exception e) {
+                GD.PrintErr($"Error loading quest definitions from DTO: {e}");
+            }
+        }
+    }
+
+    public void LoadFishDefinitions() {
+        Dictionary<string, FishDefinitionDTO>? fishDefinitions = LoadAssetFromJSON<Dictionary<string, FishDefinitionDTO>>(_fishDefinitionsPath);
+        if (fishDefinitions != null) {
+            try {
+                foreach (var kv in fishDefinitions) {
+                    InventoryItemDefinition model = new InventoryItemDefinition(kv.Value);
+                    InventoryItemDefinitions.Add(kv.Key, model);
+                }
+            }
+            catch (Exception e) {
+                GD.PrintErr($"Error loading fish definitions from DTO: {e}");
+            }
+        }
+    }
+
+    // TODO: validate referential integrity
+    // public void ValidateAssetReferrers() {}
 
     public InventoryItemDefinition GetInventoryItemDefinition(string uuid) {
         try {
