6c2fca90b60d93663ea1fecf59a9e4df622da8e0
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-03 12:13:07 -0600

    rm PersistentState, initialize inventories, expose getting player view

--- common/asset-manager/AssetManager.cs
+++ common/asset-manager/AssetManager.cs
@@ -4,11 +4,6 @@ using System.IO;
 using System.Text.Json;
 using Godot;
 
-public interface IGameAssetDTO {
-    bool IsValid();
-    string Stringify();
-}
-
 public class AssetDefinitionArray<T> {
     public T[]? Array { get; set; }
 }
@@ -20,54 +15,83 @@ public partial class AssetManager : Node {
         return _singleton;
     }
 
-    private const string _boatInventoryPath = "res://data/default-boat-inventory.json";
+    private const string _defaultBoatInventoryPath = "res://data/default-boat-inventory.json";
+    private const string _defaultQuestInventoryPath = "res://data/default-quest-inventory.json";
+    private const string _defaultStorageInventoryPath = "res://data/default-storage-inventory.json";
     private const string _fishDefinitionsPath = "res://data/fish.json";
     private const string _itemCategoryDefinitionsPath = "res://data/item-categories.json";
     private const string _questDefinitionsPath = "res://data/quests.json";
 
-    public const string TemporaryInventoryID = "INVENTORY-dc106fde-f9a0-4a99-8b58-0dee17fce491";
-    public const string PlayerOneBoatInventoryID = "INVENTORY-31cdec79-2a3b-4f4d-9e23-a878915f3973";
-    public const string PlayerOneQuestInventoryID = "INVENTORY-96c151e3-2436-406c-967c-79a1cc89c3ac";
-    public const string PlayerTwoBoatInventoryID = "INVENTORY-e158299f-a54e-42b0-964a-3ac732ec3631";
-    public const string PlayerTwoQuestInventoryID = "INVENTORY-f7091de2-6804-4f22-b8f1-d17be782adf6";
-    public const string StorageInventoryID = "INVENTORY-9f5d56ed-c8aa-4899-b749-66bd734141fb";
+    private const string _temporaryInventoryID = "INVENTORY-dc106fde-f9a0-4a99-8b58-0dee17fce491";
+    private PlayerStateAssetIDs[] _playerStateAssetIDs;
+    private const string _storageInventoryID = "INVENTORY-9f5d56ed-c8aa-4899-b749-66bd734141fb";
 
-    public Dictionary<string, Inventory> Inventories = new Dictionary<string, Inventory>();
-    public Dictionary<string, InventoryItemDefinition> InventoryItemDefinitions = new Dictionary<string, InventoryItemDefinition>();
-    public Dictionary<string, InventoryItemCategory> InventoryItemCategories = new Dictionary<string, InventoryItemCategory>();
-    public Dictionary<string, QuestDefinition> QuestDefinitions = new Dictionary<string, QuestDefinition>();
+    private InventoryDTO? _defaultBoatInventoryDTO;
+    private InventoryDTO? _defaultQuestInventoryDTO;
+    private InventoryDTO? _defaultStorageInventoryDTO;
+    private Dictionary<string, Inventory> _inventories = new Dictionary<string, Inventory>();
+    private Dictionary<string, InventoryItemDefinition> _inventoryItemDefinitions = new Dictionary<string, InventoryItemDefinition>();
+    private Dictionary<string, InventoryItemCategory> _inventoryItemCategories = new Dictionary<string, InventoryItemCategory>();
+    private Dictionary<string, QuestDefinition> _questDefinitions = new Dictionary<string, QuestDefinition>();
 
     public AssetManager() {
         ProcessMode = ProcessModeEnum.Always;
+        _playerStateAssetIDs = new PlayerStateAssetIDs[2]{
+            new PlayerStateAssetIDs(
+                boatInventoryID: "INVENTORY-31cdec79-2a3b-4f4d-9e23-a878915f3973",
+                questInventoryID: "INVENTORY-96c151e3-2436-406c-967c-79a1cc89c3ac",
+                storageID: _storageInventoryID
+            ),
+            new PlayerStateAssetIDs(
+                boatInventoryID: "INVENTORY-e158299f-a54e-42b0-964a-3ac732ec3631",
+                questInventoryID: "INVENTORY-f7091de2-6804-4f22-b8f1-d17be782adf6",
+                storageID: _storageInventoryID
+            )
+        };
     }
 
     public override void _Ready() {
         _singletonTracker.Ready(this);
         LoadAssets();
+        InitializeInventories();
     }
 
     private void LoadAssets() {
-        LoadDefaultBoatInventories();
-
-        // TODO: instantiate temp inventory to size large enough for any pickup
+        _defaultBoatInventoryDTO = LoadAssetFromJSON<InventoryDTO>(_defaultBoatInventoryPath);
+        _defaultQuestInventoryDTO = LoadAssetFromJSON<InventoryDTO>(_defaultQuestInventoryPath);
+        _defaultStorageInventoryDTO = LoadAssetFromJSON<InventoryDTO>(_defaultStorageInventoryPath);
 
         LoadAssets(
-            InventoryItemDefinitions,
+            _inventoryItemDefinitions,
             _fishDefinitionsPath,
             (InventoryItemDefinitionDTO dto) => new InventoryItemDefinition(dto),
             AssetIDValidator.IsInventoryItemDefinitionID
         );
         LoadAssets(
-            InventoryItemCategories,
+            _inventoryItemCategories,
             _itemCategoryDefinitionsPath,
             (InventoryItemCategoryDTO dto) => new InventoryItemCategory(dto),
             AssetIDValidator.IsInventoryItemCategoryID
         );
-        LoadAssets(QuestDefinitions,
+        LoadAssets(_questDefinitions,
             _questDefinitionsPath,
             (QuestDefinitionDTO dto) => new QuestDefinition(dto),
             AssetIDValidator.IsQuestID
         );
+    }
+
+    private void InitializeInventories() {
+        foreach (PlayerStateAssetIDs ids in _playerStateAssetIDs) {
+            if (!_inventories.ContainsKey(ids.BoatInventoryID) && _defaultBoatInventoryDTO != null) {
+                _inventories.Add(ids.BoatInventoryID, new Inventory(_defaultBoatInventoryDTO));
+            }
+            if (!_inventories.ContainsKey(ids.QuestInventoryID) && _defaultQuestInventoryDTO != null) {
+                _inventories.Add(ids.QuestInventoryID, new Inventory(_defaultQuestInventoryDTO));
+            }
+            if (!_inventories.ContainsKey(ids.StorageID) && _defaultStorageInventoryDTO != null) {
+                _inventories.Add(ids.StorageID, new Inventory(_defaultStorageInventoryDTO));
+            }
+        }
     }
 
     private T? LoadAssetFromJSON<T>(string filePath) {
@@ -113,29 +137,36 @@ public partial class AssetManager : Node {
         }
     }
 
-    // load initial boat inventories (shape and contents)
-    private void LoadDefaultBoatInventories() {
-        InventoryDTO? boatInventory = LoadAssetFromJSON<InventoryDTO>(_boatInventoryPath);
-        if (boatInventory != null) {
-            try {
-                // take care not to assign both players the same inventory reference
-                Inventory model1 = new Inventory(boatInventory);
-                Inventory model2 = new Inventory(boatInventory);
-                Inventories.Add(PlayerOneBoatInventoryID, model1);
-                Inventories.Add(PlayerTwoBoatInventoryID, model2);
-            }
-            catch (Exception e) {
-                GD.PrintErr($"Error loading boat inventory from DTO: {e}");
-            }
-        }
-    }
-
     // TODO: validate referential integrity
     // public void ValidateAssetReferrers() {}
 
-    private InventoryItemDefinition GetInventoryItemDefinition(string uuid) {
+    /*
+    public Inventory GetTemporaryInventory() {
+
+    }
+    */
+
+    public PlayerStateView GetPlayerView(int playerIndex) {
+        if (playerIndex < 0 || playerIndex >= _playerStateAssetIDs.Length) {
+            throw new IndexOutOfRangeException($"Player index {playerIndex} is out of range");
+        }
+        PlayerStateAssetIDs assetIDs = _playerStateAssetIDs[playerIndex];
+        return new PlayerStateView(assetIDs);
+    }
+
+    public Inventory GetInventory(string uuid) {
         try {
-            return InventoryItemDefinitions[uuid];
+            return _inventories[uuid];
+        }
+        catch (KeyNotFoundException e) {
+            GD.PrintErr($"Inventory not found: {uuid}");
+            throw e;
+        }
+    }
+
+    public InventoryItemDefinition GetInventoryItemDefinition(string uuid) {
+        try {
+            return _inventoryItemDefinitions[uuid];
         }
         catch (KeyNotFoundException e) {
             GD.PrintErr($"Inventory item definition not found: {uuid}");
