0d215703f85d8e6805f54a2edd7e44afca5cf437
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-10 10:26:51 -0600

    WIP: connectedness algorithm

--- common/util/ConnectedArray.cs
+++ common/util/ConnectedArray.cs
@@ -1,21 +1,57 @@
 public static class ConnectedArray {
     // checks if an array defining a space is connected
-    // TODO: this won't guard against splitting the inventory in half
     public static bool IsArrayConnected(int width, int height, bool[] array) {
-        // Short circuit cause this is just broken
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < width; y++) {
-                if (array[y * width + x]) {
-                    bool left = width == 1 || (x > 0 && array[y * width + x - 1]);
-                    bool right = width == 1 || (x < width - 1 && array[y * width + x + 1]);
-                    bool up = height == 1 || (y > 0 && array[(y - 1) * width + x]);
-                    bool down = height == 1 || (y < height - 1 && array[(y + 1) * width + x]);
-                    if (!left && !right && !up && !down) {
-                        return false;
-                    }
-                }
+        // find the first filled tile
+        int firstFilled = -1;
+        for (int i = 0; i < width * height; i++) {
+            if (array[i]) {
+                firstFilled = i;
+                break;
+            }
+        }
+
+        bool[] visited = new bool[width * height];
+        visited[firstFilled] = true;
+        depthFirstSearch(width, height, array, firstFilled, visited);
+
+        // validate that all filled tiles are connected
+        for (int i = 0; i < width * height; i++) {
+            if (array[i] && !visited[i]) {
+                return false;
             }
         }
         return true;
+    }
+
+    private static void depthFirstSearch(int width, int height, bool[] array, int idx, bool[] visited) {
+        visited[idx] = true;
+
+        int x = idx % width;
+        int y = idx / width;
+
+        if (x > 0) {
+            int leftIdx = y * width + x - 1;
+            if (array[leftIdx] && !visited[leftIdx]) {
+                depthFirstSearch(width, height, array, leftIdx, visited);
+            }
+        }
+        if (x < width - 1) {
+            int rightIdx = y * width + x + 1;
+            if (array[rightIdx] && !visited[rightIdx]) {
+                depthFirstSearch(width, height, array, rightIdx, visited);
+            }
+        }
+        if (y > 0) {
+            int upIdx = (y - 1) * width + x;
+            if (array[upIdx] && !visited[upIdx]) {
+                depthFirstSearch(width, height, array, upIdx, visited);
+            }
+        }
+        if (y < height - 1) {
+            int downIdx = (y + 1) * width + x;
+            if (array[downIdx] && !visited[downIdx]) {
+                depthFirstSearch(width, height, array, downIdx, visited);
+            }
+        }
     }
 }
