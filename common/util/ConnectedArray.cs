f0673a4196bae7651aac36f427913ef7b3713ae0
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-10-10 10:30:24 -0600

    feat: working connectedness algorithm

--- common/util/ConnectedArray.cs
+++ common/util/ConnectedArray.cs
@@ -1,6 +1,8 @@
 public static class ConnectedArray {
     // checks if an array defining a space is connected
     public static bool IsArrayConnected(int width, int height, bool[] array) {
+        DebugTools.Assert(width * height == array.Length, "Array size does not match width and height");
+
         // find the first filled tile
         int firstFilled = -1;
         for (int i = 0; i < width * height; i++) {
@@ -11,7 +13,6 @@ public static class ConnectedArray {
         }
 
         bool[] visited = new bool[width * height];
-        visited[firstFilled] = true;
         depthFirstSearch(width, height, array, firstFilled, visited);
 
         // validate that all filled tiles are connected
@@ -24,34 +25,17 @@ public static class ConnectedArray {
     }
 
     private static void depthFirstSearch(int width, int height, bool[] array, int idx, bool[] visited) {
-        visited[idx] = true;
-
         int x = idx % width;
         int y = idx / width;
 
-        if (x > 0) {
-            int leftIdx = y * width + x - 1;
-            if (array[leftIdx] && !visited[leftIdx]) {
-                depthFirstSearch(width, height, array, leftIdx, visited);
-            }
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            return;
         }
-        if (x < width - 1) {
-            int rightIdx = y * width + x + 1;
-            if (array[rightIdx] && !visited[rightIdx]) {
-                depthFirstSearch(width, height, array, rightIdx, visited);
-            }
-        }
-        if (y > 0) {
-            int upIdx = (y - 1) * width + x;
-            if (array[upIdx] && !visited[upIdx]) {
-                depthFirstSearch(width, height, array, upIdx, visited);
-            }
-        }
-        if (y < height - 1) {
-            int downIdx = (y + 1) * width + x;
-            if (array[downIdx] && !visited[downIdx]) {
-                depthFirstSearch(width, height, array, downIdx, visited);
-            }
-        }
+        visited[idx] = true;
+
+        depthFirstSearch(width, height, array, y * width + x - 1, visited);
+        depthFirstSearch(width, height, array, y * width + x + 1, visited);
+        depthFirstSearch(width, height, array, (y - 1) * width + x, visited);
+        depthFirstSearch(width, height, array, (y + 1) * width + x, visited);
     }
 }
